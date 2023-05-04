12cb46741d0c71ec907e708e90990cdb67cab0ff
Ceydi Y. Cuenca <ceycuenca20@gmail.com>
2023-05-04 20:17:19 +0200

    Home page

--- src/app/body/body/body.component.ts
+++ src/app/body/body/body.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+//import Typewriter from 't-writer.js'
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent {
+export class BodyComponent implements  OnInit{
+  i = 0;
+  txt = "DAW";
+  speed = 150;
+
+  ngOnInit(){
+    this.typeWriter();
+  }
+
+
+ typeWriter() {
+    if (this.i < this.txt?.length) {
+      console.log(this.txt?.length)
+      document.getElementById("demo")!.innerHTML += this.txt.charAt(this.i);
+      console.log(this.txt.charAt(this.i));
+      this.i++;
+      console.log(this.i++)
+      setTimeout(this.typeWriter, this.speed);
+    }
+  };
+
+
+
+
 
 }
+
+
+
