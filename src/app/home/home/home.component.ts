01fe7b538e4cad4a8c782f2c7e863ddaadf66db6
Ceydi Y. Cuenca <ceycuenca20@gmail.com>
2023-05-16 17:29:41 +0200

    Haciendo team.

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -6,6 +6,41 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements  OnInit{
+  frontEnd = [
+    {
+      img: 'assets/img/home/HTML.png',
+      p: "Interdum habitant dapibus iaculis lacinia purus dui, nullam condimentum metus ante vehicula curabitur, nulla senectus parturient donec nisi.",
+      link: "https://www.w3schools.com/html/"
+    },
+    {
+      img: 'assets/img/home/Angular.png',
+      p: "Interdum habitant dapibus iaculis lacinia purus dui, nullam condimentum metus ante vehicula curabitur, nulla senectus parturient donec nisi.",
+      link: "https://angular.io/docs"
+    },
+    {
+      img: 'assets/img/home/React.png',
+      p: "Interdum habitant dapibus iaculis lacinia purus dui, nullam condimentum metus ante vehicula curabitur, nulla senectus parturient donec nisi.",
+      link: "https://legacy.reactjs.org/docs/getting-started.html"
+    }
+  ]
+  banckEnd =[
+    {
+      img: 'assets/img/home/PHP.png',
+      p: "Suspendisse fringilla et nisi et mattis. Curabitur sed finibus nisi. Integer nibh sapien, vehicula et auctor.",
+      link: "https://www.php.net/docs.php"
+    },
+    {
+      img: 'assets/img/home/javascript.png',
+      p: "Suspendisse fringilla et nisi et mattis. Curabitur sed finibus nisi. Integer nibh sapien, vehicula et auctor.",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+    },
+    {
+      img: 'assets/img/home/java.jpg',
+      p: "Suspendisse fringilla et nisi et mattis. Curabitur sed finibus nisi. Integer nibh sapien, vehicula et auctor.",
+      link: "https://dev.java/learn/"
+    }
+  ]
+
 
   ngOnInit(){
     this.typeWriter();
--- src/app/team/team/team.component.ts
+++ src/app/team/team/team.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent {
+  team = [
+    {
+      img: 'https://bootdey.com/img/Content/avatar/avatar7.png',
+      nombre: 'Josefina',
+      primerApellido: 'López',
+      segundoApellido: 'Gónzalez',
+      edad: '23',
+      cumple: '12-02-2000',
+      email: 'tuemail@hotmail.com',
+      pais: 'España',
+      linkedin: 'https://es.linkedin.com',
+      github: 'https://github.com/'
+    }
+  ]
 
 }
